d2c8e9c729e4f11a9fdeb3ff25d72974406459bc
CollinsD61 <dohoang61425@gmail.com>
2024-04-26 11:10:03 +0700

    chatroom

---
+++ client/Client.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace client
+{
+    partial class Client
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txbMessage = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.lsvMessage = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // txbMessage
+            // 
+            this.txbMessage.Location = new System.Drawing.Point(12, 367);
+            this.txbMessage.Multiline = true;
+            this.txbMessage.Name = "txbMessage";
+            this.txbMessage.Size = new System.Drawing.Size(631, 58);
+            this.txbMessage.TabIndex = 8;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(669, 367);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.btnSend.Size = new System.Drawing.Size(119, 58);
+            this.btnSend.TabIndex = 7;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // lsvMessage
+            // 
+            this.lsvMessage.HideSelection = false;
+            this.lsvMessage.Location = new System.Drawing.Point(12, 26);
+            this.lsvMessage.Name = "lsvMessage";
+            this.lsvMessage.Size = new System.Drawing.Size(776, 306);
+            this.lsvMessage.TabIndex = 6;
+            this.lsvMessage.UseCompatibleStateImageBehavior = false;
+            this.lsvMessage.View = System.Windows.Forms.View.List;
+            // 
+            // Client
+            // 
+            this.AcceptButton = this.btnSend;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txbMessage);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.lsvMessage);
+            this.Name = "Client";
+            this.Text = "Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txbMessage;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.ListView lsvMessage;
+    }
+}
+
---
+++ server/Server.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace server
+{
+    public partial class Server : Form
+    {
+        public Server()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+            Connect();
+        }
+
+        private void server_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Close();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)// gửi tin cho tất cả client
+        {
+            foreach(Socket item in ClientList) {
+                Send(item);
+            }
+            AddMessage(txbMessage.Text);
+            txbMessage.Clear();
+        }
+
+        IPEndPoint IP;
+        Socket server;
+        List<Socket> ClientList;
+
+        void Connect()// kết nối 
+        {
+            ClientList = new List<Socket>();
+            IP = new IPEndPoint(IPAddress.Any, 9854);
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+
+            server.Bind(IP);
+            Thread Listen = new Thread(() =>
+            {
+                try
+                {
+                    while (true)
+                    {
+                        server.Listen(100);
+                        Socket client = server.Accept();
+                        ClientList.Add(client);
+
+                        Thread receive = new Thread(Receive);
+                        receive.IsBackground = true;
+                        receive.Start(client);
+                    }
+                }
+                catch {
+                    IP = new IPEndPoint(IPAddress.Any, 2224);
+                    server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+                }
+            });
+
+            Listen.IsBackground = true;
+            Listen.Start();
+        }
+
+
+        void Close()
+        {
+            server.Close();
+        }
+
+
+        void Send(Socket client ) // gửi tin 
+        {
+            if (client!= null &&  txbMessage.Text != string.Empty)
+                client.Send(Serializse(txbMessage.Text));
+        }
+
+        void Receive(object obj)    // nhận tin
+        {
+            Socket client = obj as Socket;
+            try
+            {
+                while (true)
+                {
+                    byte[] data = new byte[1024 * 5000];
+                    client.Receive(data);
+                    string message = (string)Deserialize(data);
+                    foreach (Socket item in ClientList)
+                    {
+                        if (item != null && item != client)
+                          item.Send(Serializse(message));
+                    }
+                    AddMessage(message);
+                }
+            }
+            catch
+            {
+                ClientList.Remove(client);
+                client.Close();
+            }
+
+
+        }
+
+
+
+        //add message vào khung chat 
+        void AddMessage(string s)
+        {
+            lsvMessage.Items.Add(new ListViewItem() { Text = s });
+            
+        }
+
+
+        //phân mảnh 
+
+        byte[] Serializse(object obj)
+        {
+            MemoryStream stream = new MemoryStream();
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            formatter.Serialize(stream, obj);
+
+            return stream.ToArray();
+        }
+
+        //gom mảnh 
+
+        object Deserialize(byte[] data)
+        {
+
+            MemoryStream stream = new MemoryStream(data);
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            return formatter.Deserialize(stream);
+
+
+        }
+
+        private void client_FormClosed(object sender, FormClosedEventArgs e) // đóng kết nối khi đóng form 
+        {
+            Close();
+        }
+    }
+}
+
